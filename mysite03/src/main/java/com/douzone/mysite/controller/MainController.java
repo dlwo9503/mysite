0e076484904ce2580ea7b1b7245e88f17031e8ef
dlwo9503 <dlwo9503@naver.com>
2021-06-10 10:25:44 +0900

    메세지 컨버터

--- mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -6,15 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.douzone.mysite.service.SiteService;
 import com.douzone.mysite.vo.SiteVo;
+import com.douzone.mysite.vo.UserVo;
 
 @Controller
 public class MainController {
 	
 	@Autowired
-	ServletContext application; // application scope 사용
+	private ServletContext application; // application scope 사용
 	
 	@Autowired
 	private SiteService siteService;
@@ -26,6 +28,21 @@ public class MainController {
 		application.setAttribute("title", siteVo.getTitle()); // application scope 사용, 서버가 돌아가는 동안 값이 유지되고 어디서든 사용 가능
 		
 		return "main/index";
-		
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg1")
+	public String message1() {
+		return "안녕하세요";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg2")
+	public Object message2() {
+		UserVo vo = new UserVo();
+		vo.setNo(1L);
+		vo.setEmail("dlwo9503@naver.com");
+		vo.setName("이재성");
+		return vo;
 	}
 }
